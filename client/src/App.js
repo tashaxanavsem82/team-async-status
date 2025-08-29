9b844047828a2748a76c5eac0131fb6d67f6b6bb
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-28 20:43:43 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Team Async Status</h1>
+        <p>Welcome to the app!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
